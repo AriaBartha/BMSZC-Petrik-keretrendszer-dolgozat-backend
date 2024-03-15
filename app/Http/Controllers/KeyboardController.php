1e10740e8cd476f4ef28885756bd932a63eca05c
Bartha Ariana <ariabartha@gmail.com>
2024-03-15 19:17:53 +0100

    Keyboard model létrehozva

---
+++ app/Http/Controllers/KeyboardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Keyboard;
+use App\Http\Requests\StoreKeyboardRequest;
+use App\Http\Requests\UpdateKeyboardRequest;
+
+class KeyboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKeyboardRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Keyboard $keyboard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Keyboard $keyboard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKeyboardRequest $request, Keyboard $keyboard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Keyboard $keyboard)
+    {
+        //
+    }
+}
